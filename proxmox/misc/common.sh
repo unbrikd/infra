8b31220c95e06fac54117a2264478176399b8963
dacarvalho-unbrikd <dacarvalho@unbrikd.com>
2025-01-26 23:38:40 +0000

    let colors and formatting to be automatically sourced when calling common script
    Signed-off-by: dacarvalho-unbrikd <dacarvalho@unbrikd.com>

--- proxmox/misc/common.sh
+++ proxmox/misc/common.sh
@@ -1,5 +1,48 @@
+# Common informations to standardize the output of the scripts
 YEAR=$(date +%Y)
 FOOTER="Unbrikd, ${YEAR} (c)"
+
+# Colors
+YW=$(echo "\033[33m")
+YWB=$(echo "\033[93m")
+BL=$(echo "\033[36m")
+RD=$(echo "\033[01;31m")
+BGN=$(echo "\033[4;92m")
+GN=$(echo "\033[1;92m")
+DGN=$(echo "\033[32m")
+
+# Formatting
+CL=$(echo "\033[m")
+UL=$(echo "\033[4m")
+BOLD=$(echo "\033[1m")
+BFR="\\r\\033[K"
+HOLD=" "
+TAB="  "
+
+# Icons
+CM="${TAB}✔️${TAB}${CL}"
+CROSS="${TAB}✖️${TAB}${CL}"
+INFO="${TAB}💡${TAB}${CL}"
+OS="${TAB}🖥️${TAB}${CL}"
+OSVERSION="${TAB}🌟${TAB}${CL}"
+CONTAINERTYPE="${TAB}📦${TAB}${CL}"
+DISKSIZE="${TAB}💾${TAB}${CL}"
+CPUCORE="${TAB}🧠${TAB}${CL}"
+RAMSIZE="${TAB}🛠️${TAB}${CL}"
+SEARCH="${TAB}🔍${TAB}${CL}"
+VERIFYPW="${TAB}🔐${TAB}${CL}"
+CONTAINERID="${TAB}🆔${TAB}${CL}"
+HOSTNAME="${TAB}🏠${TAB}${CL}"
+BRIDGE="${TAB}🌉${TAB}${CL}"
+NETWORK="${TAB}📡${TAB}${CL}"
+GATEWAY="${TAB}🌐${TAB}${CL}"
+DISABLEIPV6="${TAB}🚫${TAB}${CL}"
+DEFAULT="${TAB}⚙️${TAB}${CL}"
+MACADDRESS="${TAB}🔗${TAB}${CL}"
+VLANTAG="${TAB}🏷️${TAB}${CL}"
+ROOTSSH="${TAB}🔑${TAB}${CL}"
+CREATING="${TAB}🚀${TAB}${CL}"
+ADVANCED="${TAB}🧩${TAB}${CL}"
 
 function header_info {
   clear
